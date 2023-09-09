ea01000d2c8a0eedd989a7af2642e4ef8446bc0c
aisha153 <144536927+aisha153@users.noreply.github.com>
2023-09-09 16:16:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['35683677', 'me', true],
+  ['35683677', 'AM', true], 
   [''] 
 ] //Number of owners
 
